b0d58351b09c64b7a2eaed29311c086a1029d995
monica <monibrave1@gmail.com>
2023-09-30 00:04:17 -0300

    try to add swagger

--- src/main/java/com/mindhub/Homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/Homebanking/HomebankingApplication.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Set;
 
 @SpringBootApplication
+//@SpringBootApplication(scanBasePackages = "com.mindhub.Homebanking")
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
